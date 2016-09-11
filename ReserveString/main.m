baa47309ae41b440ce2f8f9d6cd9cc474b84daed
Paul Wood III <paul.wood.iii@gmail.com>
2016-09-11 15:23:59 -0400

    Initial Commit

---
+++ ReserveString/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  ReserveString
+//
+//  Created by Paul Wood on 9/11/16.
+//  Copyright © 2016 Paul Wood. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
